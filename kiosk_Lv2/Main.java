1fe20407a31ac3d4f14a0c4a19a115a205dfe931
임PRO스터\su992 <su9926@naver.com>
2024-11-23 00:19:43 +0900

    :sparkles: Apply collection to access menu list

--- kiosk_Lv2/Main.java
+++ kiosk_Lv2/Main.java
@@ -2,13 +2,40 @@ package kiosk_Lv2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<MenuItem> menuList=new ArrayList<>();
+        List<MenuItem> menuList=new ArrayList<>(); //음식 메뉴를 담는 list 선언, 초기화
+        /*list에 음식 메뉴 추가*/
         menuList.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         menuList.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         menuList.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuList.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        Scanner sc=new Scanner(System.in);
+        int command=Integer.MAX_VALUE;
+
+        /*메뉴판 출력*/
+        do {
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i = 0; i < menuList.size(); i++) { //list에 있는 메뉴 출력
+                System.out.println(i + ".  " + menuList.get(i).foodName + "   | W "
+                        + menuList.get(i).prize + " | " + menuList.get(i).description);
+            }
+            System.out.println("0. 종료      | 종료");
+
+            command = sc.nextInt();
+            if (command == 0) { // 입력된 숫자가 0이면 프로그램 종료
+                System.out.println("키오스크를 종료합니다.");
+            }else if(command<=menuList.size()){ // 메뉴에 있는 숫자가 입력되면 이름, 가격, 설명 출력
+                System.out.println(command + ".  " + menuList.get(command).foodName + "   | W "
+                        + menuList.get(command).prize + " | " + menuList.get(command).description+"\n");
+            }else{
+                System.out.println("메뉴판에 없는 번호입니다."+"\n");
+            }
+        }while (command!=0);
+
+
     }
 }
