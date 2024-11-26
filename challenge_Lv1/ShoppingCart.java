6474ddfc4a6d7c300e4f63e8424d0cec901d482a
임PRO스터\su992 <su9926@naver.com>
2024-11-26 17:11:58 +0900

    :sparkles: Create ShoppingCart class

--- challenge_Lv1/ShoppingCart.java
+++ challenge_Lv1/ShoppingCart.java
@@ -1,5 +1,27 @@
 package challenge_Lv1;
 
 public class ShoppingCart {
+    private String itemName; //음식 이름
+    private int quantity; //장바구니에 담긴 음식의 수량
+    private Double prize; //음식 가격
 
+    /*생성자*/
+    public ShoppingCart(String itemName, Double prize, int quantity){
+        this.itemName=itemName;
+
+        if(prize>=0.0) this.prize=prize;
+        else this.prize=0.0; //prize에는 양수만 저장될 수 있음
+
+        if(quantity>=0) this.quantity=quantity;
+        else this.quantity=0; //quantity에 음수는 저장할 수 없음. 최솟값은 0.
+    }
+
+    public void setQuantity(int quantity) {
+        if(quantity>=0) this.quantity = quantity;
+        else this.quantity=0; //quantity에 음수는 저장할 수 없음. 최솟값은 0.
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
