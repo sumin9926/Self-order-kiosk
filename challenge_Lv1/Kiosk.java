ebfb25bfdd7da5e618bb215ae0dd71a6dce6dbef
임PRO스터\su992 <su9926@naver.com>
2024-11-27 10:35:30 +0900

    :sparkles: Add method for ordering menu items

--- challenge_Lv1/Kiosk.java
+++ challenge_Lv1/Kiosk.java
@@ -7,6 +7,7 @@ import java.util.*;
  * menus 정보는 Main으로부터 전달받음
  */
 public class Kiosk {
+    Scanner sc=new Scanner(System.in);
     private Map<String, ShoppingCart> shoppingCarts=new HashMap<>(); //선택한 메뉴를 담을 장바구니 컬렉션
     private static List<Menu> menus=new ArrayList<>(); //Menu 객체를 저장하는 List
 
@@ -16,23 +17,33 @@ public class Kiosk {
 
     /*키오스크 프로그램 실행 메서드*/
     void start(){
-        int command = Integer.MAX_VALUE; //사용자로부터 입력받은 숫자를 저장하는 변수
-
         do {
+            int command = Integer.MAX_VALUE; //사용자로부터 입력받은 숫자를 저장하는 변수
             /*1. 카테고리 선택 단계*/
             System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
             showCategory();//카테고리 목록 표시
             if(!shoppingCarts.isEmpty()) showOrderMenu(); //장바구니에 메뉴가 담겨있으면 ORDER MENU도 같이 출력
-
             command = scanNumber(menus);//사용자로부터 카테고리 번호 입력 받기,(유효하지 않은 숫자 예외 처리)
             if (command == 0) { //입력된 숫자가 0일경우 키오스크 종료
                 System.out.println("키오스크를 종료합니다.");
                 return;
-            } else showMenuItems(command); //입력된 카테고리 번호에 맞는 음식 메뉴 리스트 출력
+            } else if(command<=menus.size()) showMenuItems(command); //입력된 카테고리 번호에 맞는 음식 메뉴 리스트 출력
+            else if(command==menus.size()+1) {
+                orderItems(); //장바구니 확인 및 주문 진행
+                continue; //메뉴판으로 돌아감
+            }else{
+                shoppingCarts.clear(); //진행중인 주문 취소
+                continue;
+            }
 
             /*2. 음식 메뉴 선택 단계*/
             List<MenuItem> menuItemsList=menus.get(command-1).getMenuItems();// 입력된 숫자에 맞는 카테고리의 음식 메뉴 List
-            command = scanNumber(menuItemsList); //사용자로부터 선택하려는 메뉴 번호 입력 받기,(유효하지 않은 숫자 예외 처리)
+            do {
+                command = sc.nextInt();//사용자로부터 선택하려는 메뉴 번호 입력 받기
+                if(command<=menuItemsList.size()) break; // 유효한 숫자 입력시 반복문 종료
+                else System.out.println("메뉴판에 없는 번호입니다."); // 유효하지 않은 숫자 입력시 예외 메시지 출력
+            }while(true);
+
             if (command != 0) {//선택된 메뉴 정보 출력
                 MenuItem menuItem=menuItemsList.get(command-1); // 입력된 숫자에 맞는 음식 메뉴 하나
                 System.out.println("선택한 메뉴: "+menuItem.getFoodName()+"   | W "
@@ -40,8 +51,6 @@ public class Kiosk {
                 /*3. 장바구니에 음식 담기*/
                 addToCart(menuItem);
             }
-            System.out.println(menuItemsList.getClass().getSimpleName());
-            System.out.println(menus.getClass().getSimpleName());
             //음식 메뉴 선택 단계에서 0 입력시 카테고리 선택 단계로 되돌아감
         }while (true);
     }
@@ -74,12 +83,10 @@ public class Kiosk {
     }
 
     /*사용자로부터 숫자를 입력 받아 유효성을 검증하는 메서드*/
-    private <T extends Collection<?>> int scanNumber(T t){ // 카테고리 혹은 음식 메뉴 리스트를 인자로 받음
-        Scanner sc=new Scanner(System.in);
+    private int scanNumber(List<Menu> t){ // 카테고리 리스트를 인자로 받음
         do{
             int num=sc.nextInt(); //숫자 입력
-            /*if(t instanceof )
-            else*/ if((shoppingCarts.isEmpty()&&num>t.size()) || (!shoppingCarts.isEmpty()&&num>t.size()+2)){ //입력받은 숫자가 유효하지 않을경우 예외 메시지 출력
+            if((shoppingCarts.isEmpty()&&num>t.size()) || (!shoppingCarts.isEmpty()&&num>t.size()+2)){ //입력받은 숫자가 유효하지 않을경우 예외 메시지 출력
                 System.out.println("메뉴판에 없는 번호입니다.");
             } else return num; //유효한 숫자일 경우 반복 종료 및 숫자 반환
         }while(true);
@@ -90,7 +97,6 @@ public class Kiosk {
         System.out.println("\""+item.getFoodName()+"   | W "+item.getPrize()+" | "+item.getDescription()+"\"");
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1. 확인        2. 취소");
-        Scanner sc=new Scanner(System.in);
         int num=Integer.MAX_VALUE; //입력받은 숫자를 저장할 변수
         do{
             num=sc.nextInt(); // 숫자 입력 받기
@@ -109,4 +115,34 @@ public class Kiosk {
             System.out.println(item.getFoodName()+" 이 장바구니에 추가되었습니다."); //저장 완료 메시지 출력
         }
     }
+
+    /*장바구니 메뉴 주문을 진행하는 메서드*/
+    private void orderItems(){
+        Double totalPrize=0.0;
+        /*1. 장바구니에 담긴 메뉴 출력*/
+        System.out.println("[ Orders ]");
+        for(ShoppingCart cartItem: shoppingCarts.values()){ //장바구니에 담기 모든 메뉴 출력
+            System.out.println(cartItem.getItemName()+" | W "
+                    +cartItem.getPrize()+" | 총 수량: "+cartItem.getQuantity());
+            totalPrize+=cartItem.getPrize()*cartItem.getQuantity(); //모든 메뉴의 가격 합 계산
+        }
+        /*2. 장바구니 메뉴 가격 합계 출력*/
+        System.out.println("\n[ Total ]");
+        System.out.println("W "+totalPrize);
+
+        /*3. 주문 혹은 보류 단계*/
+        System.out.println("\n1. 주문      2. 메뉴판");
+        int num=Integer.MAX_VALUE; //입력받은 숫자를 저장할 변수
+        do {
+            num = sc.nextInt();//사용자로부터 번호 입력 받기
+            if(num==1 || num==2) break; // 유효한 숫자 입력시 반복문 종료(1. 주문, 2. 메뉴판)
+            else System.out.println("메뉴판에 없는 번호입니다."); // 유효하지 않은 숫자 입력시 예외 메시지 출력
+        }while(true);
+        if(num==1) { //1 입력시 주문 진행
+            System.out.println("주문이 완료되었습니다. 금액은 W "+totalPrize+" 입니다.\n");
+            shoppingCarts.clear(); //장바구니 초기화
+        }
+    }
+
 }
+
